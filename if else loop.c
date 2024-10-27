50d0cda94ad103e120a095ba6d9ed90b5fb43437
Aman Raj <160530800+rajamannn@users.noreply.github.com>
2024-10-27 21:39:57 +0530

    Add files via upload

---
+++ if else loop.c
@@ -0,0 +1,16 @@
+//program to print if else statement
+#include <stdio.h>
+
+int main()
+{
+  int t = 20;
+
+  if (t < 18)
+  {
+    printf("Good day.");
+  } else
+  {
+    printf("Good evening.");
+  }
+  return 0;
+}
